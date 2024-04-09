91ed7a6a62f3435f22747caf68a29f5e7403cde7
herberdu <0xherbertdu@gmail.com>
2024-04-09 22:11:17 +0800

    Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
-// const { message, createDataItemSigner, connect, dryrun, result } = require('@permaweb/aoconnect');
-import { dryrun} from '@permaweb/aoconnect';
-import React, { useState, useEffect } from 'react';
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-const Home = () => {
-    const [data, setData] = useState(null);
+import Home from './Home';
 
-    async function getDry() {
-        return await dryrun({
-            process: 'Fhm5tFTmN3EXBji6EngRhu1g22DgM7SGi7uQBopZMlc',
-            tags: [{ name: 'Action', value: 'Eval' }],
-            data: "require('json').encode(myName)",
-            Owner: 'b8FjTeLN55rWw9M-Vt6fpUhD1OMsXbdD8g2ta2L9Woo',
-        });
-    }
-
-    useEffect(() => {
-        getDry().then((result) => {
-            setData(result.Output.data.output);
-        });
-    }, []);
-
-    if (!data) {
-        return <div>Loading...</div>;
-    }
-
+function App() {
     return (
-        <main>
-            <div>
-                <h1>首页</h1>
-                {data && (
-                    <div>
-                        <p>{data}</p>
-                    </div>
-                )}
-            </div>
-            <button onClick={() => getDry()}>Dry</button>
-        </main>
+        <HashRouter>
+            <Routes>
+                <Route path={'/'} element={<Home />} />
+            </Routes>
+        </HashRouter>
     );
-};
+}
 
-export default Home;
+export default App;
